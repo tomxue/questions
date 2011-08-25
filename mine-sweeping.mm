826739902ecfde902adfd699acddd55c2c0c9bf9
maemo <maemo@maemo-TomXue>
2011-08-25 15:58:24 +0100

    add random feature

---
+++ mine-sweeping.mm
@@ -0,0 +1,221 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1313939656349" ID="ID_1316525964" MODIFIED="1314080231662" STYLE="fork" TEXT="Science &amp; Technology">
+<edge COLOR="#99ff66"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1313939656349" ID="ID_171039710" MODIFIED="1314062247517" POSITION="right" STYLE="fork" TEXT="Computer">
+<edge COLOR="#3366ff" STYLE="sharp_bezier"/>
+<node CREATED="1313940556345" ID="ID_1624272006" MODIFIED="1314080247627" TEXT="Linux">
+<edge COLOR="#ff99ff" STYLE="sharp_bezier"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1313940804848" ID="ID_902479942" MODIFIED="1314240421597" TEXT="HW">
+<node CREATED="1313940753015" ID="ID_1021462733" MODIFIED="1314080249733" TEXT="IRQ">
+<node CREATED="1313940757943" ID="ID_1379356069" MODIFIED="1314064654245" TEXT="How does irq work">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940808585" ID="ID_5218694" MODIFIED="1314080249734" TEXT="Camera">
+<node CREATED="1313940817360" ID="ID_1062442460" MODIFIED="1314064659701" TEXT="How does qv4l2 work? Can I learn something from it">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313941155673" ID="ID_369887745" MODIFIED="1314064669450" TEXT="How does luvcview work">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940856627" ID="ID_1663812660" MODIFIED="1314080249736" TEXT="N900">
+<node CREATED="1313940862645" ID="ID_1569453918" MODIFIED="1314064674096" TEXT="How to get the coordinate data of N900 touch panel?">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977670453" ID="ID_733424789" MODIFIED="1314064679162" TEXT="Where is the test pads&apos; position of N900 serial port">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313977684133" ID="ID_1198068916" MODIFIED="1314080249736" TEXT="MCU">
+<node CREATED="1313977688261" ID="ID_826615121" MODIFIED="1314064683765" TEXT="What is the working principle of watch dog">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313977709238" ID="ID_1822696456" MODIFIED="1314240440429" TEXT="Protocol">
+<node CREATED="1313977720614" ID="ID_1581067962" MODIFIED="1314064687878" TEXT="What is ULPI interface">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977733590" ID="ID_52808100" MODIFIED="1314064691640" TEXT="What is the difference between USB OTG and USB host">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1314240421598" ID="ID_1997030457" MODIFIED="1314240431749" TEXT="FM Radio">
+<node CREATED="1314240442920" ID="ID_1091896948" MODIFIED="1314240474341" TEXT="How to make N9&apos;s FM radio work">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1314240456920" ID="ID_844576243" MODIFIED="1314240470000" TEXT="What is the working principle of N900">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1313940624017" ID="ID_866776191" MODIFIED="1314080248456" TEXT="OS">
+<node CREATED="1313939829594" ID="ID_1393005867" MODIFIED="1314080249737" TEXT="How to setup a workable Linux/GNU system">
+<icon BUILTIN="help"/>
+<node CREATED="1313941120037" ID="ID_186511471" MODIFIED="1314240438117" TEXT="Boot">
+<node CREATED="1313941124738" ID="ID_1394472854" MODIFIED="1314065172170" TEXT="How does uboot work">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1314065175490" ID="ID_317653846" MODIFIED="1314065185469" TEXT="How to compile uboot">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1313940603379" ID="ID_709614372" MODIFIED="1314080248456" TEXT="Porting">
+<node CREATED="1313939788615" ID="ID_348202826" MODIFIED="1314065193282" TEXT="How to port Linux to a new HW">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313939933392" ID="ID_561441034" MODIFIED="1314080248457" TEXT="meltemi">
+<node CREATED="1313939967167" ID="ID_1154323171" MODIFIED="1314065199694" TEXT="How to program in meltemi">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313939972791" ID="ID_944134865" MODIFIED="1314065206951" TEXT="What is the difference between meltemi and Linux ">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940128876" ID="ID_678368250" MODIFIED="1314080248458" TEXT="Progress">
+<node BACKGROUND_COLOR="#ffffff" CREATED="1313940138533" ID="ID_523020627" MODIFIED="1314065212054" TEXT="In what situation shall we use workqueue/timer/wait_queue">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940692959" ID="ID_1383906873" MODIFIED="1314080248458" TEXT="Thread">
+<node BACKGROUND_COLOR="#ffffff" CREATED="1313940702065" ID="ID_290839921" MODIFIED="1314065245168" TEXT="How to apply multi-threads method on printing A and B tasks simultaneously">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940192430" ID="ID_600664361" MODIFIED="1314080248459" TEXT="App">
+<node CREATED="1313940196775" ID="ID_730555004" MODIFIED="1314065250941" TEXT="How to use iwconfig">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977774423" ID="ID_695272116" MODIFIED="1314065374410" TEXT="How to use aircrack to get the WIFI password">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313941076747" ID="ID_927733341" MODIFIED="1314080248460" TEXT="FS">
+<node CREATED="1313941081075" ID="ID_1685609865" MODIFIED="1314065389933" TEXT="What is the purpose of udev">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313941098409" ID="ID_1107481167" MODIFIED="1314065394168" TEXT="What is the difference between FILE and file">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940579414" ID="ID_268997002" MODIFIED="1314080248461" TEXT="File format">
+<node CREATED="1313940587764" ID="ID_1595303360" MODIFIED="1314065416322" TEXT="What is ELF">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1314065409928" ID="ID_1917451308" MODIFIED="1314065435199" TEXT="What is the difference between ELF and Windows&apos; file format">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940264749" ID="ID_126035784" MODIFIED="1314080248462" TEXT="Docs">
+<node CREATED="1313940272692" ID="ID_271429933" MODIFIED="1314065440485" TEXT="What is said in the Linux documentation">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940784764" ID="ID_1515364041" MODIFIED="1314080248463" TEXT="Vmware">
+<node CREATED="1313940789006" ID="ID_301912214" MODIFIED="1314065447238" TEXT="How to solve the sound problem(no sound inside of Vmware) of Vmware">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313940950738" ID="ID_1624714353" MODIFIED="1314080248463" TEXT="GUI">
+<node CREATED="1313940963477" ID="ID_137311025" MODIFIED="1314065452039" TEXT="How to make a GTK program&#x9;">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313941012574" ID="ID_105500101" MODIFIED="1314080248464" TEXT="Windows">
+<node CREATED="1313941016708" ID="ID_156049520" MODIFIED="1314065456397" TEXT="What is the working principle of wine">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1313977555266" ID="ID_1123305599" MODIFIED="1314111040679" TEXT="Language">
+<edge COLOR="#66ccff"/>
+<node CREATED="1313977565875" ID="ID_1090765436" MODIFIED="1314080248465" TEXT="C++">
+<node CREATED="1313977571763" ID="ID_1959221418" MODIFIED="1314065461182" TEXT="How does C++ realize OOP">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977601092" ID="ID_933923827" MODIFIED="1314065464684" TEXT="What is template in C++">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313977613124" ID="ID_302604162" MODIFIED="1314080248466" TEXT="Java">
+<node CREATED="1313977618372" ID="ID_1177177674" MODIFIED="1314065468676" TEXT="How to make(IDE, compile, run) a simple Java program">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313977632948" ID="ID_1976357007" MODIFIED="1314080248466" TEXT="Qt">
+<node CREATED="1313977636708" ID="ID_1179689706" MODIFIED="1314065473484" TEXT="How to statically link Qt exe file">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977650133" ID="ID_1483967797" MODIFIED="1314065477828" TEXT="How to make a Qt container program">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1314111042928" ID="ID_1661172072" MODIFIED="1314111047424" TEXT="Python">
+<node CREATED="1314111060771" ID="ID_1865092004" MODIFIED="1314111073025" TEXT="How to make a simple python program">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1313940297227" ID="ID_1455407160" MODIFIED="1314080247628" TEXT="X Window">
+<node CREATED="1313940311383" ID="ID_427402068" MODIFIED="1314065482532" TEXT="What is the working principle of X Window">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313941178805" ID="ID_1536657092" MODIFIED="1314080247629" TEXT="Free BSD">
+<node CREATED="1313977484449" ID="ID_1086429820" MODIFIED="1314065486723" TEXT="How to install FreeBSD">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977506754" ID="ID_14934291" MODIFIED="1314065493230" TEXT=" What is the difference between FreeBSD and Linux">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1313977903953" ID="ID_1206171526" MODIFIED="1314024594842" POSITION="left" TEXT="Physics">
+<edge COLOR="#9999ff" STYLE="sharp_bezier"/>
+<node CREATED="1313977911866" ID="ID_365286862" MODIFIED="1314064614475" TEXT="What is the conversion efficiency of the solar cell">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977936378" ID="ID_1837412019" MODIFIED="1314064608852" TEXT="How is the gravity field twisted">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977951275" ID="ID_764745608" MODIFIED="1314064602570" TEXT=" What is the relationship between them">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1313977966955" ID="ID_895067823" MODIFIED="1314064581405" TEXT="Why the nuclear fission is stable">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1314064510022" ID="ID_1300694122" MODIFIED="1314064551801" TEXT="What is the working principle of holography">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1313977981547" ID="ID_1687671901" MODIFIED="1314022841181" POSITION="left" TEXT="Biology">
+<edge COLOR="#9999ff" STYLE="sharp_bezier"/>
+<node CREATED="1313977990987" ID="ID_1113021620" MODIFIED="1314064565628" TEXT="Why does mule cannot breed">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1314024942435" ID="ID_458701723" MODIFIED="1314235257193" POSITION="left" TEXT="Work">
+<edge COLOR="#ffcccc"/>
+<node CREATED="1313978010987" ID="ID_991953821" MODIFIED="1314235365000" TEXT="Project">
+<edge COLOR="#33ff33" STYLE="sharp_bezier"/>
+<node CREATED="1313978016012" ID="ID_360998280" MODIFIED="1314064619880" TEXT="How to use Bluetooth serial port">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1314023035160" ID="ID_444516874" MODIFIED="1314080247631" TEXT="IPR">
+<node CREATED="1314023046870" ID="ID_1009251107" MODIFIED="1314064646749" TEXT="USB charging">
+<icon BUILTIN="forward"/>
+</node>
+</node>
+</node>
+</node>
+</map>
